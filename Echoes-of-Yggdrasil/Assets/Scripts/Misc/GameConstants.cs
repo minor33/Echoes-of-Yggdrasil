5884dc5ab82c62adefb138212151c034f5891253
NamesAreHard6600 <namesarehard6600@gmail.com>
2025-11-09 00:45:58 -0800

    Keyword Comments

--- Echoes-of-Yggdrasil/Assets/Scripts/Misc/GameConstants.cs
+++ Echoes-of-Yggdrasil/Assets/Scripts/Misc/GameConstants.cs
@@ -13,14 +13,15 @@ public enum Target {
 }
 
 public enum Keyword {
-    DAMAGE,
-    BLOCK,
-    TARGET,
-    DRAW,
-    DUPLICATE,
-    INVOKE,
-    STABLE,
-    NULL
+    DAMAGE,     // Do damage to target
+    BLOCK,      // Add block to self. Currently not implemented with TARGET keyword. 
+    TARGET,     // Intended for use with other keywords which have targets, mainly damage and block (not implemented). 
+                // Use before the keyword. Can be used multiple times for different targets. Should default to front enemy in all cases. 
+    DRAW,       // Card draw
+    DUPLICATE,  // Intended for play abilities, will probably add duplicate to the the next card played if used in a rage effect.
+    INVOKE,     // Intended for rage abilities, will probably add invoke to the first card tirggered in the rage queue if in a play effect. 
+    STABLE,     // Intended for rage abilities, will not work outside of them. 
+    NULL        // Empty keyword for misc. use
 }
 
 public enum EnemyAction {
